2933076d6ed19de5dadb1e4285d2446d6f6b7413
Sith Saly <32166233+ThomasVSaly@users.noreply.github.com>
2019-10-20 14:02:21 +1000

    week display feature mealplan page

--- src/views/Mealplan.vue
+++ src/views/Mealplan.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="mealplan">
     
-    <h5>This week 
-      <span>
-        <i v-if="show" class="material-icons"></i>
-        <i v-else class="material-icons"></i>
+    <h5 @click="show = !show">This week 
+      <span class="icon-box">
+        <i v-if="show" class="material-icons">arrow_drop_down</i>
+        <i v-else class="material-icons">arrow_right</i>
       </span>
     </h5>
-    <div>
+    <div v-if="show">
       <CardRow name="Monday" :influencerMealplan="false" />
       <CardRow name="Tuesday" :influencerMealplan="false" />
       <CardRow name="Wednesday" :influencerMealplan="false" />
@@ -16,6 +16,15 @@
       <CardRow name="Saturday" :influencerMealplan="false" />
       <CardRow name="Sunday" :influencerMealplan="false" />
     </div>
+
+    <h5 @click="show2 = !show2">Next week 
+      <span class="icon-box">
+        <i v-if="show2" class="material-icons">arrow_drop_down</i>
+        <i v-else class="material-icons">arrow_right</i>
+      </span>
+    </h5>
+    <p v-if="show2">There are currently no meals to show</p>
+
   </div>
 </template>
 
@@ -29,8 +38,24 @@ export default {
   },
   data() {
     return {
-      show: true
+      show: true,
+      show2: false
     }
   }
 }
 </script>
+
+<style scoped>
+.mealplan > h5 {
+  margin-left: 0.75rem;
+  margin-bottom: 1rem;
+  display: flex;
+}
+.mealplan > p {
+  margin-left: 0.75rem;
+}
+.icon-box {
+  display: flex;
+  align-items: center;
+}
+</style>
